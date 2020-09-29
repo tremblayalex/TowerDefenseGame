3f935cd0e9ce0fc4c4390a2c065cf19584c5bf17
PEPE\etien <etienneperinet@hotmail.ca>
2020-09-29 16:29:49 -0400

    Clic dans le game area

--- Assets/ClicManager/ClickManager.cs
+++ Assets/ClicManager/ClickManager.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetAxis("Fire1") != 0)
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
             DoesItHits(hit);
-            //Debug.Log("Mouse Clicked");
+            
+
         }
+       
     }
 
     void DoesItHits(RaycastHit2D hit)
@@ -30,6 +33,7 @@ public class ClickManager : MonoBehaviour
         {
             Debug.Log("Something was clicked!");
             Debug.Log(hit.collider.gameObject.name);
+
             //hit.collider.attachedRigidbody.AddForce(Vector2.up);
             //hit.collider.gameObject.GetComponent<OnClic>(OnClic).;
         }
@@ -39,3 +43,79 @@ public class ClickManager : MonoBehaviour
         }
     }
 }
+
+
+//Solution à Alex pour le clic. Il 
+/*
+ 
+     using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OnClic : MonoBehaviour
+{
+    public Camera camera;
+    private Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            Vector2 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPos = tilemap.WorldToCell(mousePos);
+            if (tilemap.HasTile(gridPos))
+            {
+                Debug.Log(tilemap.GetTile(gridPos).name);
+            }
+            
+            //RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            //Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            //if (hit != null)
+            //{
+            //    Transform objectHit = hit.transform;
+
+            //    DoesItHits(hit);
+            //    // Do something with the object that was hit by the raycast.
+            //}
+            
+        }
+    }
+
+    void DoesItHits(RaycastHit2D hit)
+{
+    if (hit.collider != null)
+    {
+        Debug.Log("Grass was clicked!");
+        //Debug.Log(hit.collider.gameObject.name);
+        //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+        //hit.collider.gameObject.GetComponent<OnClic>(OnClic).;
+    }
+    else
+    {
+        Debug.Log("Mouse Clicked");
+    }
+}
+
+public void ClicFunction()
+{
+
+}
+public void GoToFirstMap()
+{
+    //OnClic.LoadScene("SampleScene");
+}
+}
+
+
+
+*/
---
+++ Assets/ClicManager/ScriptOnClic/OnClic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OnClic : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+
+    public void ClicFunction()
+    {
+
+    }
+    public void GoToFirstMap()
+    {
+        //OnClic.LoadScene("SampleScene");
+    }
+}
