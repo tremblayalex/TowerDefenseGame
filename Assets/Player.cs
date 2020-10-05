c379fc06e62ab6e14efbc451cf95559469d3feff
Frederic_Guay <fredericguay@hotmail.ca>
2020-10-05 19:20:14 -0400

    Création de la classe joueur et changement de texte par fonction

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int Vie = 100;
+    public int Argent = 100;
+    
+
+    void Mourir()
+    {
+        TextMeshProUGUI myText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        if (Vie > 0)
+        {
+            Vie = Vie - 1;
+        }
+        if (Vie <= 0)
+        {
+            myText.text = "Vous êtes mort";
+        }
+        else
+        {
+            myText.text = Vie.ToString();
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            Mourir();
+        }
+    }
+}
