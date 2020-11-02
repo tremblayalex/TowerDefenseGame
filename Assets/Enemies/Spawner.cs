be05710080afcdcc396570247d8d29e26d6e967a
ouzinem@hotmail.com <ouzinem@hotmail.com>
2020-11-02 12:46:08 -0500

    tank variant non focntionnel

--- Assets/Enemies/Spawner.cs
+++ Assets/Enemies/Spawner.cs
@@ -10,8 +10,8 @@ public class Spawner : MonoBehaviour
     public GameObject enemyPrefab;
     public float waveDuration =60f;
     public float delayBetweenWaves = 15f;
-    public int firstWaveEnemyCount = 10;
-    public float waveEnemyIncrementMultiplier = 1.2f;
+    public int firstWaveEnemyCount = 3;
+    public float waveEnemyIncrementMultiplier = 2f;
     
 
     private float delayBeforeNextSpawn = 0f;
@@ -42,28 +42,43 @@ public class Spawner : MonoBehaviour
 
         delayBeforeNextSpawn -= Time.deltaTime;
     }
-
-     IEnumerator SpawnAWave(int compteur ,float timeBetwenEnnemi)
-     {
      
-        for (int i = 0; i < compteur; i++)
-        { 
+     IEnumerator SpawnAWave(int nombreEnnemie, float timeBetwenEnnemi)
+     {
 
-            SpawnNewEnemy();
+        //for (int i = 0; i < compteur; i++)     
+
+        while (nombreEnnemie <= 0)
+        {
+            CustomWave(ref nombreEnnemie);
+            
             yield return new WaitForSeconds(timeBetwenEnnemi);
 
         }
         waveNumber++;
-        
+
         print("wave is finish");
 
      }
 
-    private void SpawnNewEnemy()
+    private void CustomWave(ref int nombreEnnemi)
     {
+        if (nombreEnnemi > (ennemiCount/2))
+        {
+
+        }
+        SpawnNewEnemy(1);
+        nombreEnnemi--;
+    }
+
+    private void SpawnNewEnemy(int indexEnnemi)
+    {
+        //generer random entre 0 et numero wave -1
+        //ensuite chercher le scriptable object 
         GameObject newEnemy = Instantiate(enemyPrefab, gameObject.transform.position, gameObject.transform.rotation);
-        newEnemy.GetComponent<Enemy>().setHitPoints(enemyScriptableObjects[0].hitPoints);
-        newEnemy.GetComponent<Enemy>().setMovementSpeed(enemyScriptableObjects[0].movementSpeed);
-        newEnemy.GetComponent<SpriteRenderer>().sprite = enemyScriptableObjects[0].enemySprite;
+        newEnemy.GetComponent<Enemy>().setHitPoints(enemyScriptableObjects[indexEnnemi].hitPoints);
+
+        newEnemy.GetComponent<Enemy>().setMovementSpeed(enemyScriptableObjects[indexEnnemi].movementSpeed);
+        newEnemy.GetComponent<SpriteRenderer>().sprite = enemyScriptableObjects[indexEnnemi].enemySprite;
     }
 }
