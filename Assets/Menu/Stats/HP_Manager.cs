d238b8f83a635330daa7cec48abf43f71632a12f
tremblayalex <tremblayalex999@gmail.com>
2020-11-09 14:38:02 -0500

    Before merging Fred's UI elements

---
+++ Assets/Menu/Stats/HP_Manager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP_Manager : MonoBehaviour
+{
+    public int Vie = 10;
+    public GameObject deathCanvasPrefab;
+    public GameObject deathCanvasPrefabInstantiation;
+
+    public void LoseHP(int amount)
+    {
+        TextMeshProUGUI myText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        Vie = Vie - amount;
+        myText.text = Vie.ToString();
+        if (Vie <= 0)
+        {
+            myText.text = "0";
+            Time.timeScale = 0;
+            deathCanvasPrefabInstantiation = Instantiate(deathCanvasPrefab, new Vector3(0, 0, 0), gameObject.transform.rotation);
+            deathCanvasPrefabInstantiation.layer = 5;
+        }
+    }
+
+    public void LoseHP()
+    {
+        TextMeshProUGUI myText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        Vie = Vie - 1;
+        myText.text = Vie.ToString();
+        if (Vie <= 0)
+        {
+            myText.text = "0";
+            Time.timeScale = 0;
+            deathCanvasPrefabInstantiation = Instantiate(deathCanvasPrefab, new Vector3(0, 0, 0), gameObject.transform.rotation);
+            deathCanvasPrefabInstantiation.layer = 5;
+        }
+    }
+}
--- Assets/Menu/Stats/Money_Manager.cs
+++ Assets/Menu/Stats/Money_Manager.cs
@@ -7,13 +7,7 @@ using UnityEngine;
 public class Money_Manager : MonoBehaviour
 {
     public int money = 300;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Minus))
